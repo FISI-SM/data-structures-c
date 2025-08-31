707bfaffd01df1efd85ee9b54ef08e1528f60cc2
GabPlus1 <158597541+GabPlus1@users.noreply.github.com>
2025-08-31 00:17:03 -0500

    Update alocacao_dinamica_de_array3d.c

--- laboratory/lab01-punteros y memoria/alocacao-dinamica-de-memoria/alocacao_dinamica_de_array3d.c
+++ laboratory/lab01-punteros y memoria/alocacao-dinamica-de-memoria/alocacao_dinamica_de_array3d.c
@@ -6,25 +6,25 @@ void matriz3d(int a, int b, int c ) {
 
     int*** matriz;
 
-    int linhas = a;
-    int colunas = b;
+    int filas = a;
+    int columnas = b;
     int profundidades = c;
 
-    matriz = (int***) malloc(sizeof(int**) * linhas);
+    matriz = (int***) malloc(sizeof(int**) * filas);
 
-    for(int i=0; i<linhas; i++) {
-        matriz[i] = (int**) malloc(sizeof(int*) * colunas);
+    for(int i=0; i<filas; i++) {
+        matriz[i] = (int**) malloc(sizeof(int*) * columnas);
     }
 
-    for ( int i=0; i<linhas; i++){ 
-       for (int j=0 ; j<colunas; j++){ 
+    for ( int i=0; i<filas; i++){ 
+       for (int j=0 ; j<columnas; j++){ 
             matriz[i][j] = (int*) malloc (sizeof (int) * profundidades);
         }   
     }
 
     int cont = 1;
-    for (int i=0; i<linhas; i++) {
-       for (int j=0; j<colunas; j++) {
+    for (int i=0; i<filas; i++) {
+       for (int j=0; j<columnas; j++) {
           for (int k=0; k<profundidades; k++) {
              matriz[i][j][k] = cont;
              cont++;
@@ -41,9 +41,9 @@ void matriz3d(int a, int b, int c ) {
     //printf("%d ", matriz[0][2][0]); 
 
     printf("{ \n");
-    for (int i=0; i<linhas; i++) {
+    for (int i=0; i<filas; i++) {
         printf("   { \n");
-        for (int j=0; j<colunas; j++) {
+        for (int j=0; j<columnas; j++) {
             printf("      { ");
            for (int k=0; k<profundidades; k++) {
               printf("%d ", matriz[i][j][k]);        
@@ -54,22 +54,19 @@ void matriz3d(int a, int b, int c ) {
      }
      printf("} \n\n");  
 
-  // inicio liberacao da matriz
-    for (int i=0 ; i<linhas ; i++){
-        for (int j=0; j<colunas ; j++){
+  // inicio liberación de la matriz
+    for (int i=0 ; i<filas ; i++){
+        for (int j=0; j<columnas ; j++){
             free ( matriz[i][j] );
         }
     } 
-   for (int i=0; i<linhas; i++){
+   for (int i=0; i<filas; i++){
         free (matriz[i]);
     }
     free (matriz);
 }
 
 int main() {  
-  
     matriz3d(4, 3, 2);
-
     return 0;
-
 }
