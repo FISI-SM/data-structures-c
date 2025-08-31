bb9275bb10e6a53d6ae140cdb6c0621910545020
GabPlus1 <158597541+GabPlus1@users.noreply.github.com>
2025-08-31 00:48:57 -0500

    Update ponteiro_array.c

--- laboratory/lab01-punteros y memoria/ponteiros-e-arrays/ponteiro_array.c
+++ laboratory/lab01-punteros y memoria/ponteiros-e-arrays/ponteiro_array.c
@@ -2,22 +2,22 @@
 
 int main() {
 
-    int vetor[5] = {5, 10, 15, 20, 25};
+    int vector[5] = {5, 10, 15, 20, 25};
 
-    int *p = vetor;
+    int *p = vector;
 
-    //o nome de um vetor é um ponteiro para o primeiro índice.  
-    printf("&vetor   = %p \n", &vetor);
-    printf("vetor    = %p \n", vetor);
-    printf("vetor[0] = %p \n", &vetor[0]);
-    printf("p        = %p \n\n", p);
+    //el nombre de un vector es un puntero al primer indice.  
+    printf("&vector   = %p \n", &vector);
+    printf("vector    = %p \n", vector);
+    printf("vector[0] = %p \n", &vector[0]);
+    printf("p         = %p \n\n", p);
 
-    printf("*vetor  = %d \n", *vetor);
-    printf("*p      = %d \n\n", *p);
+    printf("*vector  = %d \n", *vector);
+    printf("*p       = %d \n\n", *p);
 
    
     for (int i = 0;i < 5;i++){
-       printf("&vetor[%d] = %p | vetor[%d] = %d \n", i, &vetor[i], i, vetor[i]);
+       printf("&vector[%d] = %p | vector[%d] = %d \n", i, &vector[i], i, vector[i]);
        printf("p+%d     = %p | p+%d    = %d \n\n", i, p+i, i, *(p + i));
     }
 
