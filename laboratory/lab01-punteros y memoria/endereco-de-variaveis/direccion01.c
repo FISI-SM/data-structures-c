66168685c4d147aa417fcd5a0022350d3c48c8df
GabPlus1 <158597541+GabPlus1@users.noreply.github.com>
2025-08-31 01:22:13 -0500

    Update and rename endereco01.c to direccion01.c

--- laboratory/lab01-punteros y memoria/endereco-de-variaveis/direccion01.c
+++ laboratory/lab01-punteros y memoria/endereco-de-variaveis/direccion01.c
@@ -4,21 +4,21 @@ int main() {
    
     /*
     La memoria RAM (Random Access Memory) de cualquier computadora es una secuencia de bytes, 
-    donde cada byte almacena 8 bits de información y es una dirección de memoria. 
+    donde cada byte almacena 8 bits de información y es una direccion de memoria. 
     Estas direcciones utilizan el sistema hexadecimal.
 
     Decimal:  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, ..., n - 1.
     Hexadecimal: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F, 10, 11, 12, 13, ..., n - 1.
 
-    Una variable es un espacio o una posición, localizada en la memoria de la computadora capaz de almacenar un valor/contenido. 
-    Toda variable tiene un nombre, un tipo de dato, un valor/contenido y una dirección de memoria. 
+    Una variable es un espacio o una posicion, localizada en la memoria de la computadora capaz de almacenar un valor/contenido. 
+    Toda variable tiene un nombre, un tipo de dato, un valor/contenido y una direccion de memoria. 
     Ocupa una cantidad de bytes dependiendo de su tipo de dato.
 
     Los tipos de datos pueden ser char que ocupa 1 byte, int ocupa 4 bytes, double ocupa 8 bytes, entre otros.
 
-    El operador & antes de la variable devuelve la dirección de memoria de la misma, esa dirección es 
+    El operador & antes de la variable devuelve la direccion de memoria de la misma, esa direccion es 
     del primer byte de la secuencia de bytes ocupada por la variable.
-    La función sizeof devuelve el número de bytes para un determinado tipo de dato.     
+    La funcion sizeof devuelve el numero de bytes para un determinado tipo de dato.     
     */   
     
     char a = 'A'; 
