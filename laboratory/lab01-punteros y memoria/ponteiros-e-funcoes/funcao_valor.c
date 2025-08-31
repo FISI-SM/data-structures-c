fc34d14fec7563622d6625df86e9914bf4e6022c
GabPlus1 <158597541+GabPlus1@users.noreply.github.com>
2025-08-31 00:56:20 -0500

    Update funcao_valor.c

--- laboratory/lab01-punteros y memoria/ponteiros-e-funcoes/funcao_valor.c
+++ laboratory/lab01-punteros y memoria/ponteiros-e-funcoes/funcao_valor.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 
-//Exemplo de funções com passagem de parâmetros por valor 
+//Ejemplo de funciones con paso de parametros por valor 
 
-int soma_escalar(int valor_x, int valor_y) {
-    int soma = valor_x + valor_y;
+int suma_escalar(int valor_x, int valor_y) {
+    int suma = valor_x + valor_y;
      
-    printf("--- DENTRO DA FUNÇÃO --- \n");
+    printf("--- DENTRO DE LA FUNCION --- \n");
     printf("&valor_x = %p | valor_x = %d\n", &valor_x, valor_x);
     printf("&valor_y = %p | valor_y = %d\n", &valor_y, valor_y);
-    printf("&soma    = %p | soma    = %d\n\n", &soma, soma); 
+    printf("&suma    = %p | suma    = %d\n\n", &suma, suma); 
 
-    return soma;
+    return suma;
 }
 
 int main() {
     
     /*
-    Funções são blocos de instruções(códigos) que pode ser nomeadas e chamadas em qualquer parte de um programa.
-    Toda função tem um nome, um tipo que determina seu valor de retorno e pode ter ou não ter parâmetros. 
-    Também não pode declarar uma função dentro de outra função.    
+    Las funciones son bloques de instrucciones(codigos) que pueden ser nombradas y llamadas en cualquier parte de un programa.
+    Toda funcion tiene un nombre, un tipo que determina su valor de retorno y puede o no tener parametros. 
+    Tambien no se puede declarar una funcion dentro de otra funcion.    
     */
 
     int valor_a = 20;
     int valor_b = 30;
     int resultado;
     
-    printf("--- ANTES DE CHAMAR A FUNÇÃO --- \n");
+    printf("--- ANTES DE LLAMAR A LA FUNCION --- \n");
     printf("&valor_a   = %p | valor_a   = %d\n", &valor_a, valor_a);
     printf("&valor_b   = %p | valor_b   = %d\n", &valor_b, valor_b);
     printf("&resultado = %p | resultado = %d\n\n", &resultado, resultado);
 
-    resultado = soma_escalar(valor_a, valor_b);
+    resultado = suma_escalar(valor_a, valor_b);
 
-    printf("--- DEPOIS DE CHAMAR A FUNÇÃO --- \n");
+    printf("--- DESPUES DE LLAMAR A LA FUNCIÓN --- \n");
     printf("&valor_a   = %p | valor_a = %d\n", &valor_a, valor_a);
     printf("&valor_b   = %p | valor_b = %d\n", &valor_b, valor_b);
     printf("&resultado = %p | valor_c = %d\n\n", &resultado, resultado);
