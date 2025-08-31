db8eefe9c653727663e83d843323f77cc4eba1d3
GabPlus1 <158597541+GabPlus1@users.noreply.github.com>
2025-08-31 00:36:06 -0500

    Update endereco02.c

--- laboratory/lab01-punteros y memoria/endereco-de-variaveis/endereco02.c
+++ laboratory/lab01-punteros y memoria/endereco-de-variaveis/endereco02.c
@@ -8,14 +8,13 @@ int main() {
     long d = 4; 
     double e = 5.;
 
-     // & obtem o endereço de memória das váriaveis.
-     // hexadecimal | decimal | conteúdo
+     // & obtiene la direccion de memoria de las variables.
+     // hexadecimal | decimal | contenido
     printf("&a = %p | &a = %ld | a = %c\n", &a, &a, a);
     printf("&b = %p | &b = %ld | b = %d\n", &b, &b, b);
     printf("&c = %p | &c = %ld | c = %d\n", &c, &c, c);
     printf("&d = %p | &d = %ld | d = %d\n", &d, &d, d);
     printf("&e = %p | &d = %ld | e = %lf\n\n", &e, &e, e);
-  
 
     return 0;
 }
