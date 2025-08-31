301109d4b779b0c9ad0914ff23649fd2620b1c94
GabPlus1 <158597541+GabPlus1@users.noreply.github.com>
2025-08-31 16:48:49 -0500

    Update and rename ordenacao.c to ordenacion.c

--- laboratory/lab01.1-recursividad y ordenacion/ordenacao/ordenacion.c
+++ laboratory/lab01.1-recursividad y ordenacion/ordenacao/ordenacion.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,13 +5,13 @@
 
 int M = 0;
 
-struct aluno{
+struct alumno{
     int matricula;
-    char nome[30];
-    float n1,n2,n3;
+    char nombre[30];
+    float n1, n2, n3;
 };
 
-void printfVet(int *V  , int N){
+void printfVet(int *V, int N){
     int i;
     for(i = 0; i < N; i++)
         printf("%2d ",V[i]);
@@ -21,7 +20,7 @@ void printfVet(int *V  , int N){
 
 //============================================
 
-void bubbleSort(int *V  , int N){
+void bubbleSort(int *V, int N){
     int i, continua, aux;
     do{
         continua = 0;
@@ -36,11 +35,11 @@ void bubbleSort(int *V  , int N){
     }while(continua);
 }
 
-void bubbleSort2(int *V  , int N){//otimizado
-    int i, continua, aux, fim = N;
+void bubbleSort2(int *V, int N){//optimizado
+    int i, continua, aux, fin = N;
     do{
         continua = 0;
-        for(i = 0; i < fim-1; i++){
+        for(i = 0; i < fin-1; i++){
             if (V[i] > V[i+1]){
                 aux = V[i];
                 V[i] = V[i+1];
@@ -48,7 +47,7 @@ void bubbleSort2(int *V  , int N){//otimizado
                 continua = i;
             }
         }
-        fim--;
+        fin--;
     }while(continua != 0);
 }
 //============================================
@@ -63,7 +62,7 @@ void insertionSort(int *V, int N){
 }
 //============================================
 void selectionSort(int *V, int N){
-    int i, j, menor, troca;
+    int i, j, menor, intercambio;
     for(i = 0; i < N-1; i++){
         menor = i;
         for(j = i+1; j < N; j++){
@@ -71,58 +70,58 @@ void selectionSort(int *V, int N){
                 menor = j;
         }
         if(i != menor){
-            troca = V[i];
+            intercambio = V[i];
             V[i] = V[menor];
-            V[menor] = troca;
+            V[menor] = intercambio;
         }
     }
 }
 //============================================
-void merge(int *V, int inicio, int meio, int fim){
-    int *temp, p1, p2, tamanho, i, j, k;
-    int fim1 = 0, fim2 = 0;
-    tamanho = fim-inicio+1;
+void merge(int *V, int inicio, int medio, int fin){
+    int *temp, p1, p2, tamano, i, j, k;
+    int fin1 = 0, fin2 = 0;
+    tamano = fin-inicio+1;
     p1 = inicio;
-    p2 = meio+1;
-    temp = (int *) malloc(tamanho*sizeof(int));
+    p2 = medio+1;
+    temp = (int *) malloc(tamano*sizeof(int));
     if(temp != NULL){
-        for(i=0; i<tamanho; i++){
-            if(!fim1 && !fim2){
+        for(i=0; i<tamano; i++){
+            if(!fin1 && !fin2){
                 if(V[p1] < V[p2])
                     temp[i]=V[p1++];
                 else
                     temp[i]=V[p2++];
 
-                if(p1>meio) fim1=1;
-                if(p2>fim) fim2=1;
+                if(p1>medio) fin1=1;
+                if(p2>fin) fin2=1;
             }else{
-                if(!fim1)
+                if(!fin1)
                     temp[i]=V[p1++];
                 else
                     temp[i]=V[p2++];
             }
         }
-        for(j=0, k=inicio; j<tamanho; j++, k++)
+        for(j=0, k=inicio; j<tamano; j++, k++)
             V[k]=temp[j];
     }
     free(temp);
 }
 
-void mergeSort(int *V, int inicio, int fim){
-    int meio;
-    if(inicio < fim){
-        meio = floor((inicio+fim)/2);
-        mergeSort(V,inicio,meio);
-        mergeSort(V,meio+1,fim);
-        merge(V,inicio,meio,fim);
+void mergeSort(int *V, int inicio, int fin){
+    int medio;
+    if(inicio < fin){
+        medio = floor((inicio+fin)/2);
+        mergeSort(V,inicio,medio);
+        mergeSort(V,medio+1,fin);
+        merge(V,inicio,medio,fin);
     }
 }
 
 //============================================
 
-void insertionSortMatricula(struct aluno *V, int N){
+void insertionSortMatricula(struct alumno *V, int N){
     int i, j;
-    struct aluno aux;
+    struct alumno aux;
     for(i = 1; i < N; i++){
         aux = V[i];
         for(j=i;(j>0) && (aux.matricula<V[j-1].matricula);j--)
@@ -130,17 +129,17 @@ void insertionSortMatricula(struct aluno *V, int N){
         V[j] = aux;
     }
 }
-/*saída strcmp(str1,str2)
-    == 0: str1 é igual a str2
-     > 0: str1 vem depois de str2
-     < 0: str1 vem antes de str2
+/*salida strcmp(str1,str2)
+    == 0: str1 es igual a str2
+     > 0: str1 viene despues de str2
+     < 0: str1 viene antes de str2
 */
-void insertionSortNome(struct aluno *V, int N){
+void insertionSortNombre(struct alumno *V, int N){
     int i, j;
-    struct aluno aux;
+    struct alumno aux;
     for(i = 1; i < N; i++){
         aux = V[i];
-        for(j=i;(j>0) && (strcmp(aux.nome, V[j-1].nome) < 0);j--)
+        for(j=i;(j>0) && (strcmp(aux.nombre, V[j-1].nombre) < 0);j--)
             V[j] = V[j-1];
         V[j] = aux;
     }
@@ -152,15 +151,15 @@ void insertionSortNome(struct aluno *V, int N){
 int main(){
     int i;
 
-    struct aluno V[4] = {{2,"Andre",9.5,7.8,8.5},
+    struct alumno V[4] = {{2,"Andre",9.5,7.8,8.5},
                          {4,"Ricardo",7.5,8.7,6.8},
                          {1,"Bianca",9.7,6.7,8.4},
                          {3,"Ana",5.7,6.1,7.4}};
 
     insertionSortMatricula(V,4);
-    insertionSortNome(V,4);
+    insertionSortNombre(V,4);
     for(i = 0; i < 4; i++)
-        printf("%d) %s\n", (V[i]).matricula, V[i].nome);
+        printf("%d) %s\n", (V[i]).matricula, V[i].nombre);
 
     return 0;
 }
@@ -177,29 +176,6 @@ int main(){
 //    int vet[7] = {23,4,67,-8,21};
 //    int N = 5;
 
- //   printf("Sem ordenar:\n");
+ //   printf("Sin ordenar:\n");
 
     //bubbleSort(vet,N);//OK
-    //bubbleSort2(vet,N);//OK
-    //insertionSort(vet,N);//OK
-    //selectionSort(vet,N);//OK
- //   mergeSort(vet,0,N);//OK
-    //quickSort(vet,0,N-1);//OK
-//    for(i=0; i<N; i++)
-//        printf("%d\n",vet[i]);
-
-//==============================================================================
-//    int vet[8] = {0,23,4,67,-8,90,54,21};
-//    int N = 8;
-//
-//    printf("Sem ordenar:\n");
-//    printfVet(vet,N);
-//    printfLinha();
-//
-//    printf("Ordenado:\n");
-//    printfVet(vet,N);
-//==============================================================================
-
-
- //   return 0;
-//}
