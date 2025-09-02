3b149b2f83561af994c4ab709c89994fb293e698
GabPlus1 <158597541+GabPlus1@users.noreply.github.com>
2025-09-02 12:38:53 -0500

    Update app_main.c

--- laboratory/lab03-listas/lista-duplamente-ligada/apps/app_main.c
+++ laboratory/lab03-listas/lista-duplamente-ligada/apps/app_main.c
@@ -1,28 +1,26 @@
-#include "lista_duplamente_ligada.h"
+#include "lista_doblemente_enlazada.h"
 #include <stdio.h>
 #include <stdlib.h> 
 
-
 int main() { 
     
-   printf("\n--- Lista Duplamente Ligada ---\n\n");
+   printf("\n--- Lista Doblemente Enlazada ---\n\n");
 
-   Lista *ptr_lista = criar_lista();
+   Lista *ptr_lista = crear_lista();
 
-   imprimir_cabeca_lista(ptr_lista);
+   imprimir_cabeza_lista(ptr_lista);
 
-   adicionar_cabeca_lista(ptr_lista, 10);
-   adicionar_cabeca_lista(ptr_lista, 33);
-   adicionar_cabeca_lista(ptr_lista, 99);
+   agregar_cabeza_lista(ptr_lista, 10);
+   agregar_cabeza_lista(ptr_lista, 33);
+   agregar_cabeza_lista(ptr_lista, 99);
    
-   imprimir_cabeca_lista(ptr_lista);
+   imprimir_cabeza_lista(ptr_lista);
 
-   imprimir_cauda_lista(ptr_lista);
+   imprimir_cola_lista(ptr_lista);
 
-   //printf("%d \n", tamanho_lista(ptr_lista));
+   //printf("%d \n", tamano_lista(ptr_lista));
 
-   destruir_lista(&ptr_lista);
-     
+   destruir_lista(&ptr_lista); 
    
    return 0;
 
