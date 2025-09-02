e7e8f73fb763400f010028457960925fd0d541b3
GabPlus1 <158597541+GabPlus1@users.noreply.github.com>
2025-09-02 12:57:59 -0500

    Update and rename lista_ligada_circular.h to lista_enlazada_circular.h

---
+++ laboratory/lab03-listas/lista-ligada-circular/include/lista_enlazada_circular.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <stdbool.h> 
+
+typedef struct nodo Nodo;
+
+Nodo* crear_lista();
+void destruir_lista_enlazada(Nodo **ptr_ptr_nodo);
+
+int tamaño_lista(const Nodo *ptr_nodo);
+
+void imprimir_lista(Nodo *ptr_nodo);
+
+void agregar_inicio_lista(Nodo **ptr_ptr_nodo, int valor); 
+void agregar_final_lista(Nodo **ptr_ptr_nodo, int valor); 
+void agregar_ordenado_lista(Nodo **ptr_ptr_nodo, int valor); 
+
+bool eliminar_inicio_lista(Nodo **ptr_ptr_nodo); // 0 no elimino y 1 elimino
+bool eliminar_final_lista(Nodo **ptr_ptr_nodo); 
+bool eliminar_valor_lista(Nodo **ptr_ptr_nodo, int valor); 
+
+int buscar_valor_lista(Nodo *ptr_nodo, int valor);
+int buscar_posicion_lista(Nodo *ptr_nodo, int posicion);
